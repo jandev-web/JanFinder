b00dd735f791f8530dc8d3458b848b45ff6cbf28
jandev-web <dev@janfinder.com>
2024-12-05 14:14:00 -0500

    Added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,16 @@ import '@/styles/globals.css';
 import { UserProvider } from '@/components/UserContext'; // Import the UserProvider
 import { Amplify } from 'aws-amplify';
 import awsExports from '@/aws-exports'; // Import your Amplify configuration
+import { metadata } from './metadata'; // Import the metadata
+
 
 // Configure Amplify client-side only
 if (typeof window !== "undefined") {
   Amplify.configure(awsExports);
 }
 const inter = Inter({ subsets: ["latin"] })
+
+
 
 
 
@@ -23,6 +27,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/public/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </head>
       <body className="w-full">
         <UserProvider> 
           {children}
---
+++ src/app/metadata.ts
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: 'JanFinder',
+    icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon.png',
+      apple: '/favicon.png',
+    },
+  };
+  
