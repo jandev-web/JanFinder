dec6e50f244a351f3be8fd2341836480de016d45
jandev-web <dev@janfinder.com>
2025-03-17 11:25:40 -0400

    Fixed bug

--- src/components/pages/UnderConstructionWhole.tsx
+++ src/components/pages/UnderConstructionWhole.tsx
@@ -46,10 +46,10 @@ const UnderConstruction: React.FC = () => {
           </h1>
         )}
         <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-6 leading-relaxed">
-          Welcome to <span className="font-bold text-blue-900">Jan<span className="text-yellow-400">Finder</span></span> — the first commercial cleaning quote bidding platform. 
+          Welcome to <span className="font-bold text-blue-900">Bid<span className="text-yellow-400">2</span>Clean</span> — the first commercial cleaning quote bidding platform. 
           We&apos;re revolutionizing the way cleaning companies and customers connect. Whether you&apos;re a customer searching for quick, easy quotes 
           to hire a cleaning company, or a cleaning business looking for innovative tools to streamline operations, attract clients, 
-          and build strong customer relationships, <span className="font-bold text-blue-900">Jan<span className="text-yellow-400">Finder</span></span> has you covered.
+          and build strong customer relationships, <span className="font-bold text-blue-900">Bid<span className="text-yellow-400">2</span>Clean</span> has you covered.
         </p>
         <p className="text-base sm:text-lg lg:text-xl text-yellow-500 italic mb-6">
           Our site is under development and will be launching soon. Stay tuned for the next big thing in the cleaning industry!
